919f40184335287e60e5ea35118fb25fbd3f1fa1
sudip2222 <sudipdiu@gmail.com>
2016-12-05 07:00:41 +0600

    something change

--- app/LoginToken.php
+++ app/LoginToken.php
@@ -31,7 +31,7 @@ class LoginToken extends Model
             "<a href='{$url}'>{$url}</a>",
             function ($message) {
                 $message->to($this->user->email)
-                    ->subject('Login to Laracasts');
+                    ->subject('Login to ItConquest');
             }
         );
     }
